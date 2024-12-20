9b0c674792f6f8309718da356d0d4ffd7622c311
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-12-20 21:47:50 +0500

    Написана модель работника

--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Worker(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='ID')
+    fio = models.CharField(max_length=255, verbose_name='ФИО')
+    team = models.IntegerField(verbose_name='Номер бригады')
+    salary = models.IntegerField(verbose_name='Зарплата')
+    specialization = models.CharField(max_length=100, verbose_name='Специализация')
+
+    def __str__(self):
+        return self.fio
+
+    class Meta:
+        verbose_name = 'Работник'
+        verbose_name_plural = 'Работники'
